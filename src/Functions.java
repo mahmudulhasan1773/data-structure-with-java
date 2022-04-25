1e7729c704ce21592f90b96186f619a373275a02
mahmudulhasan1773 <mahmudulhasan177@gmail.com>
2022-04-25 07:25:01 +0600

    update function

--- src/Functions.java
+++ src/Functions.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class Functions {
     public static void main(String[] args) {
+        sum();
+    }
+
+    static void sum(){
         Scanner in = new Scanner(System.in);
         System.out.println("Enter first number");
         int fNumber = in.nextInt();
